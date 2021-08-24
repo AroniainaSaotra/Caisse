78e8ad5bc84d652c0dca853319fa818ad4beb978
Aroniaina Saotra <saotrarahajason@gmail.com>
2021-08-25 00:58:37 +0300

    change9

--- application/views/accueil.php
+++ application/views/accueil.php
@@ -8,11 +8,11 @@
         <form action="<?php echo site_url('Accueil/choix') ?>" method="POST">   
             <div class="form-group">
                 <label for="caisse"><h3>Choissir une caisse</h3></label>
-                <select class="form-control" name="caisse" id="caisse">
-                    <?php for($i=0;$i<count($caisse);$i++) { ?>
-                        <option value="<?php echo $caisse[$i]->getId() ?>"><?php echo $caisse[$i]->getNumero() ?></option>
-                    <?php } ?>
-                </select>
+                <button type="button" class="btn btn-success">Caisse 1</button>
+                <button type="button" class="btn btn-warning">Caisse 2</button>
+                <button type="button" class="btn btn-info">Caisse 3</button>
+
+
             </div>
             <Button class="btn btn-primary mb-5">Valider</Button>
         </form>     
