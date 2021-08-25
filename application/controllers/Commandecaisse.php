f617ed20ff3021abfe7957744843382c127cda3e
Aroniaina Saotra <saotrarahajason@gmail.com>
2021-08-25 15:54:50 +0300

    change13

--- application/controllers/Commandecaisse.php
+++ application/controllers/Commandecaisse.php
@@ -8,15 +8,36 @@ class Accueil extends CI_Controller {
 		parent::__construct();
 	}
 
-	public function listeFinProd()
+	public function listeFinProdNouriture()
 	{
 		$this->load->model('Produit');
 		$c=new Produit();
 		$c=$c->categorieNouriturre();
 		$data=array();
-		$data["prod"]=$c;
+		$data['prod']=$c;
 		$data['vue']='commandecaisse.php';
 		$this->load->view('template',$data);
 	}	
+
+    public function listeFinProdJouets()
+    {
+        $this->load->model('Produit');
+        $c=new Produit();
+        $c=$c->categorieJouet();
+        $data=array();
+        $data['prod']=$c;
+        $data['vue']='commandecaisse.php';
+        $this->load->view('template',$data);
+    }
+
+    public function listeFinProdTechno()
+    {
+        $this->load->model('Produit');
+        $c=new Produit();
+        $c=$c->categorieTechnologie();
+        $data['prod']=$c;
+        $data['vue']='commandecaisse.php';
+        $this->load->view('template',$data);
+    }
 	
 }
--- application/views/commandecaisse.php
+++ application/views/commandecaisse.php
@@ -1,8 +1,13 @@
 <?php
+$prod=$_GET['prod'];
 
+$total=0;
+for($i=0;$i<count($achat);$i++){
+    $total+=$achat[$i]->getMontant();
+}
 ?>
 <div class="container" style="width:400px; height:450px; border:2px solid red; text-align:center;" >
-<form action="<?php echo site_url("commandecaisse/listeFinProd") ?>">
+
     <table class="table" style="width:400px; height:450px;">  
   <caption><h2>Nouriture</h2></caption>
   <thead>
@@ -27,3 +32,61 @@
 </table>
 </form>
 </div>
+
+<div class="container" style="float:rigth; width:400px; height:450px; border:2px solid red; text-align:center; margin-left:450px; margin-top:10px;" >
+
+    <table class="table" style="width:400px; height:450px;">  
+  <caption><h2>Jouets</h2></caption>
+  <thead>
+    <tr>
+      <th scope="col">Images</th>
+      <th scope="col">Nom du Produit</th>
+      <th scope="col">Prix</th>
+    </tr>
+  </thead>
+</br>
+  <tbody>
+
+    <tr>
+    <?php for($i=0;$i<count($prod);$i++) { ?> 
+        <td> <?php echo $prod[$i]->getImages() ?></td>
+        <td> <?php echo $prod[$i]->getNom() ?></td>
+        <td> <?php echo $prod[$i]->getPrix() ?></td>  
+    <?php } ?> 
+    </tr>
+    
+  </tbody>
+</table>
+</form>
+</div>
+
+<div class="col-md-6 col-md-offset-1">
+        <b>Caisse numero <?php echo $idCaisse ?></b>
+        <table class="table table-striped table-inverse table-responsive">
+        <thead class="thead-inverse">
+                    <tr>
+                        <th>Produit</th>
+                        <th>Prix Unitaire</th>
+                        <th>Quantite</th>
+                        <th>Montant</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php for($i=0;$i<count($achat);$i++) { ?>
+                        <tr>
+                            <td scope="row"><?php echo $achat[$i]->getNom() ?></td>
+                            <td scope="row"><?php echo $achat[$i]->getPrix() ?></td>
+                            <td scope="row"><?php echo $achat[$i]->getQuantite() ?></td>
+                            <td scope="row"><?php echo $achat[$i]->getMontant() ?></td>
+                        </tr>
+                        
+                        <?php } ?>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>Total</td>
+                            <td> <?php echo $total ?></td>
+                        </tr>
+                    </tbody>
+            </table>
+    </div>
